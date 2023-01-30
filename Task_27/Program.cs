5cbb86ed790fb8259c41c42855eee3498192d2ba
Osa <safronova.2104@mail.ru>
2023-01-30 22:47:52 +0300

    Решение

---
+++ Task_27/Program.cs
@@ -0,0 +1,21 @@
+﻿// принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+System.Console.WriteLine(GetSum(new Random(). Next(1000, 1000000)));
+static int GetSum(int num)
+{
+    System.Console.WriteLine(num);
+    int sum = 0;
+    while(num > 0)
+    {
+        sum = sum + num %10;
+        num /= 10;
+    }
+    return sum; 
+}
+
+ 
+
+ 
---
+++ Task_29/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+PrintArrey(GetRandomArrey(8));
+int[] GetRandomArrey(int length)
+{
+    int [] result = new int [length];
+    for(int i = 0; i < length; i++)
+    {
+        result[i] = new Random(). Next(0, 100); 
+    }
+    return result;
+}
+void PrintArrey(int[] arr)
+{
+    foreach (int item in arr)
+    {
+       System.Console.WriteLine(item); 
+    }
+}
